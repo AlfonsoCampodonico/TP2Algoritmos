bbc6995efa2c092a837e7acd6c99ba92bc3c9411
Alfonso Campodonico <38387367+AlfonsoCampodonico@users.noreply.github.com>
2021-02-15 21:53:31 -0300

    Seguimiento

--- src/InterfazUsuario.cpp
+++ src/InterfazUsuario.cpp
@@ -41,7 +41,8 @@ void InterfazUsuario::mostrarMenuDeJuegoDeLaVida(){
     cout << "Menu Juego de la Vida 1.0 Recargado" << endl;
     cout << "1. Ejecutar un turno" << endl;
     cout << "2. Reiniciar" << endl;
-    cout << "3. Terminar" << endl << endl;
+    cout << "3. Terminar" << endl;
+    cout << "4. Seguimiento de Gen" << endl << endl;
 }
 
 void InterfazUsuario::mostrarQueNoEsUnNumeroValido(){
